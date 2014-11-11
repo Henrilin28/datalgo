40ff463d010d24b5beb3c1d10e3872579b81f810
iyed <iyed.bennour@gmail.com>
2014-11-11 18:48:21 +0100

    cleanup

--- sort.h
+++ sort.h
@@ -17,7 +17,6 @@ extern "C" {
   int part(int arr[], int start, int end);
   void quick_sort(int arr[], int begin, int end);
   int* merge(int arr1[], unsigned int size1, int arr2[], unsigned int size2);
-  int uniques(int arr[], int len);
   
 #ifdef __cplusplus
 }
